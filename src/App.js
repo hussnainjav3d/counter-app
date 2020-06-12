23db4d5747db63a4542dc01c935d22369303e927
hussnainjav3d <hussnain.jav3d@gmail.com>
2020-06-12 05:51:28 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let [ count, setCount ] = useState(0);
+	let [ isMorning, setMorning ] = useState(true);
+	return (
+		<div className={`box ${isMorning ? 'dayTime' : ''}`}>
+			<button className="nightBtn" onClick={() => setMorning(!isMorning)}>
+				Background Color : {isMorning ? 'Morning' : 'Night'}
+			</button>
+			<h1>Counter </h1>
+			<p className="valueCounter">{count}</p>
+			<div className="btnDiv">
+				<button className="incrementBtn" onClick={() => setCount(++count)}>
+					+
+				</button>
+				<button className="decrementBtn" onClick={() => setCount(--count)}>
+					-
+				</button>
+			</div>
+			<button className="resetBtn" onClick={() => setCount(0)}>
+				Reset
+			</button>
+			<div className="footer">
+				<h3>Copyright &copy; Hussnain Javed 2020</h3>
+			</div>
+		</div>
+	);
 }
 
 export default App;
